30c4729f8cbf7f5af3f5c3311e29cad27c2cf947
Jason Valdron <jason@valdron.ca>
2024-08-12 14:45:55 -0300

    Add backups
    Signed-off-by: Jason Valdron <jason@valdron.ca>

--- service_accounts.tf
+++ service_accounts.tf
@@ -76,9 +76,12 @@ module "iam_role_mt_aws_glacier" {
   inline_policy       = data.aws_iam_policy_document.mt_aws_glacier.json
 
   service_accounts = [
-    "system:serviceaccount:home-assistant:esphome-esphome",
+    "system:serviceaccount:home-assistant:esphome",
     "system:serviceaccount:home-assistant:home-assistant",
-    "system:serviceaccount:mealie:mealie"
+    "system:serviceaccount:mealie:mealie",
+    "system:serviceaccount:storage:mt-aws-glacier-backups",
+    "system:serviceaccount:storage:mt-aws-glacier-photos",
+    "system:serviceaccount:storage:mt-aws-glacier-profiles"
   ]
 }
 
